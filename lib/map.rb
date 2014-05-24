005fda24e7f92aa7289a3b814f1f414df902e029
Club09 s09t214 OOKAWA Masahiro <clc.iguto@gmail.com>
2014-05-25 00:35:47 +0900

    Mapの初期化処理を分割

--- lib/map.rb
+++ lib/map.rb
@@ -1,11 +1,27 @@
+require File.expand_path('../cell', __FILE__)
+
 class Map
+  attr_accessor :cells
+
   def initialize(size: 3, start_x: 0 , start_y: size-1, goal_x: size-1, goal_y: 0)
     @size = size
-    @cells = Array.new( @size ){ Array.new( @size ) }
-    @cells.map!{ |line| line.map!{ |cell| cell = Cell.new } }
-    @cells[start_y][start_x].start = true
-    @cells[goal_y][goal_x].goal = true
+    @cells = create_cells
+    set_start_cell(start_x, start_y)
+    set_goal_cell(goal_x, goal_y)
   end
 
-  attr_accessor :cells
+  private
+
+  def create_cells
+    cells = Array.new(@size) { Array.new(@size) }
+    cells.map! { |line| line.map! { |cell| cell = Cell.new } }
+  end
+
+  def set_start_cell(x, y)
+    @cells[y][x].start = true
+  end
+
+  def set_goal_cell(x, y)
+    @cells[y][x].goal = true
+  end
 end
