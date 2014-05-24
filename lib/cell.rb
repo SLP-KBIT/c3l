a5840a62c18a280fb5d2047524d1e52134621920
Club09 s09t214 OOKAWA Masahiro <clc.iguto@gmail.com>
2014-05-25 00:40:13 +0900

    cellの初期化処理中で､色をランダムで決める処理を追加

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,9 +1,14 @@
 class Cell
+  COLOR_CLEAR = :clear
+  COLOR_RED   = :red
+  COLOR_BLUE  = :blue
+  COLORS = [ COLOR_CLEAR, COLOR_RED, COLOR_BLUE ]
+
   attr_reader   :color
   attr_accessor :goal, :start
 
-  def initialize(color: :clear, goal: false, start: false)
-    @color = color
+  def initialize(color: nil, goal: false, start: false)
+    @color = color || COLORS.sample
     @goal = goal
     @start = start
   end
