daa97c269d2232e80a5f3ac762fe744d759183aa
Club09 s09t214 OOKAWA Masahiro <clc.iguto@gmail.com>
2014-06-16 21:52:27 +0900

    メトリクス測定用スクリプト ポートを8079へ変更

--- bin/analyze_metrics.sh
+++ bin/analyze_metrics.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 metric_fu --no-open --format html
-ruby -rwebrick -e "WEBrick::HTTPServer.new({DocumentRoot: './tmp/metric_fu/output', Port: 8080}).start"  
+ruby -rwebrick -e "WEBrick::HTTPServer.new({DocumentRoot: './tmp/metric_fu/output', Port: 8079}).start"
